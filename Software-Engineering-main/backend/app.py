70241fb4e630649b71e8be86cadc09b099ee2f88
liyingxian123 <14408706+liyingxian123@user.noreply.gitee.com>
2025-06-29 19:16:15 +0800

    第二次提交：修改了3个文件

--- Software-Engineering-main/backend/app.py
+++ Software-Engineering-main/backend/app.py
@@ -12,7 +12,7 @@ CORS(app, origins=["http://localhost:3000"])
 app.config['JWT_SECRET_KEY'] = 'a9fdg#12!@#A3fFj9z_Secret987' 
 jwt = JWTManager(app)  # ✅ 初始化 JWT 管理器
 db.init_app(app)
-
+# 这是我进行的修改
 register_routes(app)
 
 if __name__ == '__main__':
